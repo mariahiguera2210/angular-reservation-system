0c35b44a8443ad42deeeafee6e23152caf08fb84
mariahiguera2210 <angelica.higueram92@gmail.com>
2024-01-24 21:17:25 -0500

    the header, login, reservation and reservation-list components are added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reservation-system';
+
+  changeTitle(){
+    this.title = "hola mundo";
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ReservationComponent } from './component/reservation/reservation.component';
+import { ReservationListComponent } from './component/reservation-list/reservation-list.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
+  //componentes declarados, las vistas
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReservationComponent,
+    ReservationListComponent,
+    LoginComponent
   ],
+  //modulos importados
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // en providers van los servicios que necesita este modulo.
   providers: [],
+  //vista raiz de la aplicacion
   bootstrap: [AppComponent]
 })
 export class AppModule { }
