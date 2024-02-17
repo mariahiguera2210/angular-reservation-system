5ca7a1b2145aefd8316179d46df955d22f5d94aa
mariahiguera2210 <angelica.higueram92@gmail.com>
2024-02-16 22:56:06 -0500

    se agrega validador en form login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookie: CookieService) {}
 
   sendCredentials(email: string, password: string): Observable<any> {
     const body = {
       email: email,
       password: password,
     };
-    return this.http.post(
-      'http://localhost:8080/api/v1/auth/authenticate',
-      body, {responseType: 'text'})
-      .pipe(tap( (response) => console.log(response)));
+    return this.http
+      .post('http://localhost:8080/api/v1/auth/authenticate', body, {
+        responseType: 'text',
+      })
+      .pipe(
+        tap((token: string) => { // La respuesta es el token JWT como texto plano
+          console.log(token);
+          this.cookie.set('token', token, 1, '/'); // Guardar el token en las cooki
+        })
+      );
   }
 }
